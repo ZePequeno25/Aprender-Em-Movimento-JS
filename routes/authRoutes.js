04696e6c33207665fa31196766bd2d45a1d86b9c
ZePequeno25 <albuquerque.aparecido@aluno.ifsp.edu.br>
2025-10-14 23:22:42 -0300

    teste de novo

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/authController');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.post('/verify-user', authController.verifyUserForPasswordResetHandler);
+router.post('/verify_user_for_password_reset', authController.verifyUserForPasswordResetHandler);
 router.post('/reset-password', authController.resetPassword);
 /**router.get('/verify-user', authController.verifyUser);
 **/
